02a271dcc396641a3ec036125f74f7d59148d10c
ingrid183 <ingridmanzano8@gmail.com>
2022-11-23 17:12:17 -0500

    Cambios
    Cambios

--- PROYECTO_BIOINSTRU/PROYECTO_BIOINSTRU.ino
+++ PROYECTO_BIOINSTRU/PROYECTO_BIOINSTRU.ino
@@ -5,6 +5,17 @@ void setup() {
 }
 
 void loop() {
-  Serial.print(random(1, 3));
+  /*
+    Esto es para generar valores aleatorios y probar el código de python
+  */
+  Serial.print(random(1,7));
+
+  /*
+    Esto es para probar la lectura del voltaje en el pin analógico
+  */
+  //double sensorValue = analogRead(A0);
+  //sensorValue=sensorValue*5/1023;
+  //Serial.println(sensorValue);
+
   delay(500);
 }
--- autoclicker.py
+++ autoclicker.py
@@ -3,18 +3,24 @@ import time
 import pyautogui
 
 time.sleep(2)
-ser = serial.Serial("COM7", 9600, timeout=1)
+ser = serial.Serial("COM6", 9600, timeout=1)
 while ser.is_open:
-    valorSerial = str(ser.read())
+    valorSerial = str(ser.readline())
     numero = 0
-    if len(valorSerial) == 4:
-        # Valor del serial -> b'2'
-        numero = int(valorSerial[2:3])
-    
-    if numero == 1:
-        pyautogui.press('right')
-    elif numero == 2:
-        pyautogui.press('left')
 
-    print(numero)
-    time.sleep(1)
+    if not(len(valorSerial) < 4):
+        numero = float(valorSerial[2:6])
+
+        if numero >= 0.1 and numero <= 0.2:
+            pyautogui.press('right')
+        elif numero > 0.2 and numero <= 0.3:
+            pyautogui.press('left')
+        if numero > 0.3 and numero <= 0.4:
+            pyautogui.press('up')
+        elif numero > 0.4 and numero <= 0.5:
+            pyautogui.press('down')
+    else:
+        print(valorSerial)    
+
+        print('Señal recibida: ' + str(numero) + ' mV')
+        time.sleep(1)
